1866b4841e9cb4479e9827a1a0445a54bb4c1a03
Jonathan Ayala Zuñiga <jonathan_ayala2011@hotmail.com>
2023-05-12 16:05:14 -0500

    Cambios de hoy

--- ConexionBase.cs
+++ ConexionBase.cs
@@ -4,30 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace ProyectoRoles
 {
     public class ConexionBase
     {
         //conexion a a la base de datos
-        public static string cadenaConexion = "Data Source=SISTEMAS1\\SQLEXPRESS;Initial Catalog=ROLES; Integrated Security=True";
+        public static string cadenaConexion = "Data Source=10.1.0.6\\SQLEXPRESS;Initial Catalog=ROLES;User ID=sa;Password=";
         //public SqlConnection conectarBD = new SqlConnection();
 
         //public ConexionBase()
         //{
-          //  conectarBD.ConnectionString = cadenaConexion;
+        //    conectarBD.ConnectionString = cadenaConexion;
         //}
         //public void abrir()
         //{
-          //  try
-            //{
-              //  conectarBD.Open();
-                //MessageBox.Show("Conexion con la base de datos completada", "SUCCES", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            //}
-            //catch (Exception ex)
-            //{
-              //  throw new Exception("Error al abrir la base de datos", ex);
-            //}
+        //    try
+        //    {
+        //        conectarBD.Open();
+        //        MessageBox.Show("Conexion con la base de datos completada", "SUCCES", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception("Error al abrir la base de datos", ex);
+        //    }
         //}
     }
 
