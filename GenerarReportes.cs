10eaee82aaa8481631e5063518b5617a9d5850d4
Jonathan Ayala Zuñiga <jonathan_ayala2011@hotmail.com>
2023-06-02 17:29:47 -0500

    si

--- GenerarReportes.cs
+++ GenerarReportes.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.IO;
+using System.Data.SqlClient;
+using ProyectoRoles;
 
 namespace ProyectoRolesConCrystal
 {
-    internal class GenerarReportes
+    public class GenerarReportes
     {
+        SqlConnection conexion =new SqlConnection(ConexionBase.cadenaConexion);
+        public void generarPDFS()
+        {
+
+        }
     }
 }
